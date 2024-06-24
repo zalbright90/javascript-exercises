8569aaa3f313efac29fd1f2f2ab2c6380a5b4a55
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-06-24 04:41:59 -0700

    Add if..else..statement for errors with negatives

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(string, number) {
-    return string.repeat(number);
+    if (number < 0)
+        return "ERROR"
+    else
+        return string.repeat(number);
 };
 
 // Do not edit below this line
