4a291ca0eceb3c950631657e5d5bb68cc1ba9519
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-06-25 00:44:22 -0700

    Set equation for the first test, thinking of an implement for ERROR

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
