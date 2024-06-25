fd3b34009b23369d26cb26ed06a7865d0d865fac
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-06-25 01:13:59 -0700

    Set function to check for leap years
    - This exercise was actually fairly easy, math is one of my stronger suites, it's when I'm messing with nouns that don't have any true integers
    
    - Should pass all test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Create a function that determines whether or not a given year is a leap year.
+    if (typeof year !== 'number') {
+        return false;
+    }
+    // Leap years are years divisible by four
+    if (year % 4 === 0) {
+        // However, years divisible by 100 are not leap years
+        if (year % 100 === 0) {
+            // unless they are divisible by 400 (like 1600 and 2000, which were leap years)
+            return year % 400 === 0;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
