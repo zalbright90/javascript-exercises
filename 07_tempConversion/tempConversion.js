1e54da89f15f5eab625cd5afba3e0bd3ea5d821c
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-06-25 01:27:08 -0700

    Set up formula to convert to nearest tenths place decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  //  (x - 32) * 5/9 = C
+  let celsius = (fahrenheit - 32) * (5/9);
+  return Math.round(celsius * 10) / 10;  // Round to 1 decimal
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // (x * 9/5) + 32 = F
+  let fahrenheit = (celsius * 9/5) + 32;
+  return Math.round(fahrenheit * 10) / 10; // Round to 1 decimal
 };
 
 // Do not edit below this line
