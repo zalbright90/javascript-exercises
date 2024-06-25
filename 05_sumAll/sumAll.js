d7c0ca7b46f6ea18a8f24b9dbc9248c37410189f
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-06-25 00:50:09 -0700

    Set function to catch error for neg numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,14 @@
 const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
     let start = Math.min(num1, num2);
     let end = Math.max(num1, num2);
     let sum = 0;
-    for (let i = start; i <= end; i++) {
-        sum = sum + i;
-    }
-    return sum;
+        for (let i = start; i <= end; i++) {
+            sum = sum + i;
+        }
+        return sum;
 };
 
 // Do not edit below this line
