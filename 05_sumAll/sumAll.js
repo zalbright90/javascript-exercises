9c93ed3132e3087d4423b576db79b3cc9e2eb68e
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-06-25 00:53:32 -0700

    Set function to catch if number is not of the same type
    - This should would for the last test too

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,9 @@ const sumAll = function(num1, num2) {
     if (num1 < 0 || num2 < 0) {
         return "ERROR";
     }
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return "ERROR";
+    }
     let start = Math.min(num1, num2);
     let end = Math.max(num1, num2);
     let sum = 0;
