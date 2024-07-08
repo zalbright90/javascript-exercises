315eaea7a28c223420697bbbded24365f1534ccd
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-07-08 04:15:18 -0700

    Updated function to handle a 0th fib number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,21 @@
 const fibonacci = function(n) {
-    if (n <= 2) {
-      return 1;
-    } else { //Fn = Fn-1 + Fn-2
-      return fibonacci(n - 1) + fibonacci(n - 2);
+    if (n === 0) {
+      return 0;
+    } else if (n === 1 || n === 2) {
+        return 1;
     }
-  };
+
+    let fib1 = 1;
+    let fib2 = 1;
+    let fib = 0;
+
+    for (integer = 3; integer <= n; integer++) {
+        fib = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fib;
+    }
+    return fib;
+};
 
 // Do not edit below this line
 module.exports = fibonacci;
