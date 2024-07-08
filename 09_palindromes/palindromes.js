a6032cf51f1fffac8024dfd80de28745003c0d1a
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-07-08 03:41:30 -0700

    Add function to pass all palindromes true or false

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    const cleanStr = string.toLowerCase().replace(/[\W_]/g, '');
+    return cleanStr === cleanStr.split('').reverse().join('');
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
