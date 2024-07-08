b64f23280c77acf7d415a30e9d1c7c8ee6d2d2c8
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-07-08 04:33:14 -0700

    Add function with map() to search by title

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const titles = books.map(book => book.title);
 
+    return titles;
 };
 
 // Do not edit below this line
