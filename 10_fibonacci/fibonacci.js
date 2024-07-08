a127b38b98ae62252c0d20de29043ab84a2cbe9d
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-07-08 03:48:55 -0700

    First Fibonacci Sequence function
    - Thanks to Geeksforgeeks for the formula in comment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    if (n <= 2) {
+      return 1;
+    } else { //Fn = Fn-1 + Fn-2
+      return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
